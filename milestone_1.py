6470db08d6d874281dbbf2d01adfb755131d817d
ansonchf <76811877+ansonchf@users.noreply.github.com>
2022-10-08 09:14:55 +0100

    Update milestone_1.py

--- milestone_1.py
+++ milestone_1.py
@@ -1,18 +1,17 @@
 import random
 
-# Task 1: Create a list containing the names of your 5 favorite fruits
+# Create a list containing the names of your 5 favorite fruits
 word_list = ["Apple", "Orange", "Pear", "Watermelon", "Banana"]
 
-# Task 2: Choose a random word from the list
+# Choose a random word from the list
 word = random.choice(word_list)
-print(word)
 
-# Task 3: Ask the user for an input
-guess = input("Enter a single letter!")
-print(guess)
+# Ask the user for an input
+if __name__=="__main__":
+    guess = input("Enter a single letter: ")
 
-# Task 4: Check that the input is a single character and only alphabetical characters
-if len(guess) <= 1 and ((guess>='a' and guess<= 'z') or (guess>='A' and guess<='Z')):
+# Check that the input is a single character and only alphabetical characters
+if len(guess) == 1 and guess.isalpha():
     print("Good guess!")
 else:
     print("Oops! That is not a valid input.")
